9493efd78fdf3c34dbe66ec29a0f74822d6cf0e7
migdall <jn@jesusnoland.com>
2018-08-29 18:15:34 -0700

    Fixing bugs and making sure you don't fight the same people again and showing you your wins

--- main.py
+++ main.py
@@ -3,7 +3,20 @@
 import time
 import random
 
-from enemies import ENEMIES
+from enemies import ENEMIES, DEFEATED, CHALLENGED
+
+
+def is_player_closest(original, player, enemy):
+    """
+    Return True if the player is closer in number
+    to the original than the enemy.
+    """
+    player_difference = abs(original - player)
+    enemy_difference = abs(original - enemy)
+    if player_difference < enemy_difference:
+        return True
+    return False
+
 
 print """
 Welcome to Bible Heroes Challenge!\n
@@ -48,12 +61,34 @@ while exit:
     player_input = str(raw_input("777: "))
     if player_input == "exit":
         exit = False
+    elif player_input == "wins":
+        print DEFEATED
     elif player_input == "fight":
-        print "Selecting enemy."
+        print "\nSelecting enemy."
         time.sleep(1)
-        selected_enemy_index = random.randrange(0, len(ENEMIES))
-        selected_enemy_name = ENEMIES[selected_enemy_index]
+        selected_enemy_name = None
+        while selected_enemy_name == None:
+            selected_enemy_index = random.randrange(0, len(ENEMIES))
+            if ENEMIES[selected_enemy_index] in CHALLENGED:
+                continue
+            selected_enemy_name = ENEMIES[selected_enemy_index]
         print "You have challenged %s" % selected_enemy_name
+        print "\nReady..."
+        time.sleep(1)
+        print "BATTLE!!!"
+        battle_random_number = random.randrange(1, 21)
+        player_battle_number_input = int(raw_input("Pick a number between 1 and 20: "))
+        enemy_battle_number_input = random.randrange(1, 21)
+        print "\nYou chose %s" % player_battle_number_input
+        print "Calculating..."
+        time.sleep(1)
+        if is_player_closest(battle_random_number, player_battle_number_input, enemy_battle_number_input):
+            print "You win"
+            DEFEATED.append(selected_enemy_name)
+        else:
+            print "%s WINS" % selected_enemy_name
+        CHALLENGED.append(selected_enemy_name)
+
 
 if not exit:
     print "\n\nShutting down the game."
